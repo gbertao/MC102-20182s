71034130bae247c0bee4097255d2a5cb8cbfa243
Giovanni <gbertao96@gmail.com>
2018-08-14 14:00:35 -0300

    Lab01 feito

---
+++ 01/lab01.py
@@ -0,0 +1,67 @@
+total = int(input())
+
+nota = 100
+x = 0
+x = total//(nota*100)
+total = total % (nota*100)
+print(str(x)+" nota(s) de R$ "+str(nota)+",00.")
+
+nota = 50
+x = 0
+x = total//(nota*100)
+total = total % (nota*100)
+print(str(x)+" nota(s) de R$ "+str(nota)+",00.")
+
+nota = 20
+x = 0
+x = total//(nota*100)
+total = total % (nota*100)
+print(str(x)+" nota(s) de R$ "+str(nota)+",00.")
+
+nota = 10
+x = 0
+x = total//(nota*100)
+total = total % (nota*100)
+print(str(x)+" nota(s) de R$ "+str(nota)+",00.")
+
+nota = 5
+x = 0
+x = total//(nota*100)
+total = total % (nota*100)
+print(str(x)+" nota(s) de R$ "+str(nota)+",00.")
+
+nota = 2
+x = 0
+x = total//(nota*100)
+total = total % (nota*100)
+print(str(x)+" nota(s) de R$ "+str(nota)+",00.")
+
+nota = 1
+x = 0
+x = total//(nota*100)
+total = total % (nota*100)
+print(str(x)+" moeda(s) de R$ "+str(nota)+",00.")
+
+nota = 50
+x = 0
+x = total//(nota)
+total = total % (nota)
+print(str(x)+" moeda(s) de R$ 0,"+str(nota)+".")
+
+nota = 25
+x = 0
+x = total//(nota)
+total = total % (nota)
+print(str(x)+" moeda(s) de R$ 0,"+str(nota)+".")
+
+nota = 10
+x = 0
+x = total//(nota)
+total = total % (nota)
+print(str(x)+" moeda(s) de R$ 0,"+str(nota)+".")
+
+nota = 5
+x = 0
+x = total//(nota)
+total = total % (nota)
+print(str(x)+" moeda(s) de R$ 0,0"+str(nota)+".")
