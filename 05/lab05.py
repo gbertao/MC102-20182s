960d023731735e332beb49000d6df73bd2e180d0
Giovanni <gbertao96@gmail.com>
2018-09-10 07:38:23 -0300

    Lab 05

---
+++ 05/lab05.py
@@ -0,0 +1,34 @@
+n = int(input())
+lines = []
+resp = []
+
+for i in range(0, n):
+    lines.append(input())
+
+h = 0
+e = 0
+for i in range(0,n):
+    if not lines[i].isalpha():
+        if lines[i][0] == '#':
+            h+=1
+        elif not (lines[i][1:].isdigit() and lines[i][0] in ("-","+")):
+            e+=1
+        else:
+            resp.append(lines[i])
+    else:
+        resp.append(lines[i])
+
+for i in resp:
+    print(i)
+
+if h > 0:
+    if h == 1:
+        print("1 hashtag foi removida.")
+    else:
+        print(h, "hashtags foram removidas.")
+
+if e > 0:
+    if e == 1:
+        print("1 emoticon foi removida.")
+    else:
+        print(e, "emoticons foram removidas.")
